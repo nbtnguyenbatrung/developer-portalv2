22ac460102db8c8bd47fb4fc7709a8a745291cb6
trung <batrung255@gmail.com>
2025-12-10 21:23:28 +0700

    update

--- lib/auth-options.ts
+++ lib/auth-options.ts
@@ -120,5 +120,17 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: 'jwt',
     maxAge: 60 * 60 * 60 * 30, // 30 days
-  }
+  },
+    cookies: {
+        sessionToken: {
+            name: `__Secure-next-auth.session-token`,
+            options: {
+                httpOnly: true,
+                sameSite: 'lax',
+                path: '/',
+                secure: true, // Bắt buộc true trên production (HTTPS)
+            },
+        },
+    },
+  secret: process.env.NEXT_AUTH_SECRET,
 }
