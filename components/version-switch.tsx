b1acb875915355e1d65b04644a94172763f92f0b
trung <batrung255@gmail.com>
2025-12-11 15:35:11 +0700

    update

--- components/version-switch.tsx
+++ components/version-switch.tsx
@@ -44,7 +44,6 @@ export function VersionSwitcher({
     onVersionChange(version)
   }
   const getName = () => {
-      if (listDocs.length <= 1) { return null }
       const doc = listDocs.find((v) => v.slug === slug)
       return doc ? doc.name : null
   }
@@ -59,10 +58,10 @@ export function VersionSwitcher({
             <GalleryVerticalEnd className="w-4 h-4" />
           </div>
           <div className="flex flex-col gap-0.5 leading-none justify-start md:ml-3 text-left"
-               title={getName() + "/v" + selectedVersion}>
+               title={getName() + "/v" + defaultVersion}>
             <span className="font-bold">{t?.version_doc}</span>
             <span className="text-xs text-gray-500 w-[130px] truncate">{getName()}</span>
-            <span className="text-xs text-gray-500">v{selectedVersion}</span>
+            <span className="text-xs text-gray-500">v{defaultVersion}</span>
           </div>
           <ChevronsUpDown className="ml-auto w-4 h-4 text-gray-400" />
         </Button>
@@ -72,46 +71,30 @@ export function VersionSwitcher({
         align="start"
       >
           {
-              listDocs.length > 1 ?
-                  listDocs.map((doc) => (
-                      <DropdownMenuSub key={doc.id}>
-                          <DropdownMenuSubTrigger>{doc.name}</DropdownMenuSubTrigger>
-                          <DropdownMenuSubContent>
-                              {
-                                  doc.version.map((version) => (
-                                      <DropdownMenuItem
-                                          key={version}
-                                          onSelect={() => handleSelect(version)}
-                                          className="flex items-center px-4 py-2 rounded-lg text-sm hover:bg-blue-50 cursor-pointer"
-                                      >
-                                          <Link href={`/product/docs/${doc.slug}/v${version}`}
-                                                className="flex items-center px-4 py-2 rounded-lg text-sm cursor-pointer">
-                                              <span className="flex-1">v{version}</span>
-                                              {version === selectedVersion && slug === doc.slug && (
-                                                  <Check className="ml-2 w-4 h-4 text-blue-600" />
-                                              )}
-                                          </Link>
-                                      </DropdownMenuItem>
-                                  ))
-                              }
-                          </DropdownMenuSubContent>
-                      </DropdownMenuSub>
-                  )) :
-                  versions.map((version) => (
-                      <DropdownMenuItem
-                          key={version}
-                          onSelect={() => handleSelect(version)}
-                          className="flex items-center px-4 py-2 rounded-lg text-sm hover:bg-blue-50 cursor-pointer"
-                      >
-                          <Link href={`/product/docs/${slug}/v${version}`}
-                                className="flex items-center px-4 py-2 rounded-lg text-sm cursor-pointer">
-                              <span className="flex-1">v{version}</span>
-                              {version === selectedVersion && (
-                                  <Check className="ml-2 w-4 h-4 text-blue-600" />
-                              )}
-                          </Link>
-                      </DropdownMenuItem>
-                  ))
+              listDocs.map((doc) => (
+                  <DropdownMenuSub key={doc.id}>
+                      <DropdownMenuSubTrigger>{doc.name}</DropdownMenuSubTrigger>
+                      <DropdownMenuSubContent>
+                          {
+                              doc.version.map((version) => (
+                                  <DropdownMenuItem
+                                      key={version}
+                                      onSelect={() => handleSelect(version)}
+                                      className="flex items-center px-4 py-2 rounded-lg text-sm hover:bg-blue-50 cursor-pointer"
+                                  >
+                                      <Link href={`/product/docs/${doc.slug}/v${version}`}
+                                            className="flex items-center px-4 py-2 rounded-lg text-sm cursor-pointer">
+                                          <span className="flex-1">v{version}</span>
+                                          {version === defaultVersion && slug === doc.slug && (
+                                              <Check className="ml-2 w-4 h-4 text-blue-600" />
+                                          )}
+                                      </Link>
+                                  </DropdownMenuItem>
+                              ))
+                          }
+                      </DropdownMenuSubContent>
+                  </DropdownMenuSub>
+              ))
           }
 
       </DropdownMenuContent>
